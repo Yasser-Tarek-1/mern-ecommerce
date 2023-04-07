79ea3380381318e37eeaffe068c3cefb9b1dbce9
mohamed salah <moh.salah790@gmail.com>
2023-04-08 01:17:35 +0200

    handle upload response

--- backend/src/helpers/uploading.ts
+++ backend/src/helpers/uploading.ts
@@ -1,9 +1,12 @@
 import { Request, Response } from "express";
 export const uploading = (req: Request, res: Response) => {
   if (!req.file) {
-    return res.status(200).send({
-      error: "Image no no",
+    return res.status(400).send({
+      error: "You should upload image",
     });
   }
-  res.status(200).send("image uploaded successfully")
-}; 
+  res.status(200).send({
+    success: true,
+    message: "Image is uploaded and saved successfully",
+  });
+};
