0519e09d10523d634aef76ea495fa2d7b3f469e5
Mohamed Salah <moh.salah70@gmail.com>
2023-04-09 14:53:14 +0200

    addional endpoinsts for cart and favourites and preparing the backend server for the build

--- backend/src/db/Connection_DB.ts
+++ backend/src/db/Connection_DB.ts
@@ -1,7 +1,10 @@
 import mongoose from "mongoose";
+import dotenv from "dotenv";
+import path from "path";
+dotenv.config({ path: path.join(__dirname, "../../config/.env") });
 export const CONNECTION_DB = (): void => {
   mongoose
-    .connect(process.env.MONGO_URL as string)
+    .connect(process.env.MONGO_URL!)
     .then(() => console.log("Datebase is Connected successfully"))
     .catch((e) => console.log(`DATEBASE ERROR : ${e}`));
 };
--- backend/src/router/favourite.router.ts
+++ backend/src/router/favourite.router.ts
@@ -1,6 +1,7 @@
 import { Router } from "express";
 import {
   addToFavouritesList,
+  clearFavouritesList,
   getFavouritesProducts,
   removeFromFavouritesList,
 } from "../controllar/favourite.controllar";
@@ -9,4 +10,5 @@ const router = Router();
 router.route("/").get(checkAuth, getFavouritesProducts);
 router.route("/add/:productId").post(checkAuth, addToFavouritesList);
 router.route("/remove/:productId").delete(checkAuth, removeFromFavouritesList);
+router.route("/clear").delete(checkAuth, clearFavouritesList);
 export default router;
